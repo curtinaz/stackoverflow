f5191ad4f0fa491eebbb6f316791588c40ee4199
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2021-09-23 11:20:06 -0300

    528618

---
+++ 528618/index.php
@@ -0,0 +1,121 @@
+
+<?php
+
+$array = [
+"heading": {
+    "title": "Faltas",
+    "created_by_name": "Base de teste Alex Pontomais",
+    "start_date": "01/09/2018",
+    "end_date": "05/09/2018",
+    "today": "21/11/2018"
+},
+"data": [
+    [
+        {
+            "header": {
+                "name": {
+                    "title": "Nome"
+                },
+                "nis": {
+                    "title": "PIS"
+                },
+                "registration_number": {
+                    "title": "Matrícula"
+                },
+                "job_title": {
+                    "title": "Cargo"
+                },
+                "team": {
+                    "title": "Equipe"
+                },
+                "shift": {
+                    "title": "Turno"
+                },
+                "date": {
+                    "title": "Data"
+                },
+                "missing_type": {
+                    "title": "Abono"
+                },
+                "missing_motive": {
+                    "title": "Motivo"
+                }
+            },
+            "data": [
+                {
+                    "name": "Darth vader",
+                    "nis": "074.42618.31-7",
+                    "registration_number": "0001",
+                    "job_title": "Gerente geral",
+                    "team": "Administração",
+                    "shift": "0002 - turno ciclico",
+                    "date": "Sáb, 01/09/2018",
+                    "missing_type": "Não",
+                    "missing_motive": null
+                },
+                {
+                    "name": "Darth vader",
+                    "nis": "074.42618.31-7",
+                    "registration_number": "0001",
+                    "job_title": "Gerente geral",
+                    "team": "Administração",
+                    "shift": "0002 - turno ciclico",
+                    "date": "Dom, 02/09/2018",
+                    "missing_type": "Não",
+                    "missing_motive": null
+                },
+                {
+                    "name": "Darth vader",
+                    "nis": "074.42618.31-7",
+                    "registration_number": "0001",
+                    "job_title": "Gerente geral",
+                    "team": "Administração",
+                    "shift": "0002 - turno ciclico",
+                    "date": "Seg, 03/09/2018",
+                    "missing_type": "Não",
+                    "missing_motive": null
+                },
+                {
+                    "name": "Darth vader",
+                    "nis": "074.42618.31-7",
+                    "registration_number": "0001",
+                    "job_title": "Gerente geral",
+                    "team": "Administração",
+                    "shift": "0002 - turno ciclico",
+                    "date": "Ter, 04/09/2018",
+                    "missing_type": "Não",
+                    "missing_motive": null
+                },
+                {
+                    "name": "Darth vader",
+                    "nis": "074.42618.31-7",
+                    "registration_number": "0001",
+                    "job_title": "Gerente geral",
+                    "team": "Administração",
+                    "shift": "0002 - turno ciclico",
+                    "date": "Qua, 05/09/2018",
+                    "missing_type": "Não",
+                    "missing_motive": null
+                }
+            ],
+            "footer": null,
+            "totals": [
+                {
+                    "title": "Total",
+                    "total": 5
+                },
+                {
+                    "title": "Total abonado",
+                    "total": 0
+                }
+            ]
+        }
+    ]
+],
+"meta": {
+    "now": 1542808555,
+    "ip": "127.0.0.1"
+}
+]
+
+?>
